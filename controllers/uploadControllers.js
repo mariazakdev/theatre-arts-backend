fe0fbcf9d66fa108e038b1f0a6b5d2ffa9985450
Maria Zak <mzakart@gmail.com>
2023-10-30 19:25:27 -0400

    Photo uplaod works in AWS, still needs to be coded more to create user

--- controllers/uploadControllers.js
+++ controllers/uploadControllers.js
@@ -1,8 +1,15 @@
 const knex = require('knex')(require('../knexfile'));
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const upload = require('../s3Config');
 
 exports.uploadData = async (req, res) => {
     try {
+
+        // Multer middleware
+        upload.single('file')(req, res, async (err) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
         const { firebaseId, photoUrl, videoUrl, description, name } = req.body;
         const [user] = await knex('users').where({ firebase_auth_id: firebaseId });
         
@@ -34,6 +41,9 @@ exports.uploadData = async (req, res) => {
             customer: customer.id,
             collection_method: "send_invoice"
         });
+
+
+    });
 
     } catch (error) {
         res.status(500).json({ error: 'Failed to upload' });
---
+++ s3Config.js
@@ -0,0 +1,28 @@
+// s3Config.js
+require("dotenv").config();
+const { S3Client } = require("@aws-sdk/client-s3");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+
+const s3 = new S3Client({
+  region: process.env.REGION,
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  },
+});
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.S3_BUCKET_NAME,
+    acl: "public-read",
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString() + "_" + file.originalname);
+    },
+  }),
+});
+
+module.exports = upload;
