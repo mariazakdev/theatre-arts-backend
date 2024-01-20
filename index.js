5b78f4890e0704b356106842a65c92388b6c913b
Maria Zak <mzakart@gmail.com>
2024-01-20 07:14:46 -0500

    updated cors

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-require('dotenv').config(); 
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const knex = require('knex')(require('./knexfile'));
-const errorMiddleware = require('./middlewares/errorHandlingMiddleware');
-const requestIdMiddleware = require('./middlewares/requestIdMiddleware');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const knex = require("knex")(require("./knexfile"));
+const errorMiddleware = require("./middlewares/errorHandlingMiddleware");
+const requestIdMiddleware = require("./middlewares/requestIdMiddleware");
 
 function createApp() {
   const app = express();
   app.use(errorMiddleware);
   app.use(requestIdMiddleware);
 
-  app.use(express.static('public'));
+  app.use(express.static("public"));
 
-  const uploadRoutes = require('./routes/uploadRoutes');
-  const paymentRoutes = require('./routes/paymentRoutes');
-  const usersFBRoutes = require('./routes/usersFirabaseRoutes');
-  const sunKingRoutes = require('./routes/sunKingRoutes');
+  const uploadRoutes = require("./routes/uploadRoutes");
+  const paymentRoutes = require("./routes/paymentRoutes");
+  const usersFBRoutes = require("./routes/usersFirabaseRoutes");
+  const sunKingRoutes = require("./routes/sunKingRoutes");
 
   app.use(
     cors({
@@ -28,26 +28,23 @@ function createApp() {
 
   app.use(bodyParser.json());
 
-  app.get('/', (req, res) => {
-    res.send('Hello, World!');
+  app.get("/", (req, res) => {
+    res.send("Hello, World!");
   });
 
-  app.use('/users', usersFBRoutes);
-  app.use('/contestants', uploadRoutes);
-  app.use('/payment', paymentRoutes);
-  app.use('/sun-king', sunKingRoutes);
-
- 
+  app.use("/users", usersFBRoutes);
+  app.use("/contestants", uploadRoutes);
+  app.use("/payment", paymentRoutes);
+  app.use("/sun-king", sunKingRoutes);
 
   return app;
 }
 
-const port = process.env.PORT || 8000; 
+const port = process.env.PORT || 8000;
 const URL = process.env.CORS_ORIGIN;
 const app = createApp();
 
-
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== "test") {
   app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`);
   });
